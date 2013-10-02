0c1847f9b1ccb6cbb0ff8999b2e961f7d6fe4664
Jackson Chen <jackson.jl.chen@gmail.com>
2013-10-02 16:12:09 -0700

    Changed instruction as per bundle author
    * updates bundle installs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 VUNDLE_DIR="$HOME/.vim/bundle/vundle"
-cp .vimrc ~/
+
+
 if [ ! -d "{$VUNDLE_DIR}" ];then
     echo "Cloning Vundle"
     git clone http://github.com/gmarik/vundle.git "$VUNDLE_DIR"
-    vim +BundleInstall +qa
 fi
+cp .vimrc ~/
+vim +BundleInstall +qall
