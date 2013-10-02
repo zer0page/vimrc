8a16089bc653d1b620d411db7d9b5e17ac96a581
Jackson Chen <jackson.jl.chen@gmail.com>
2013-10-02 16:14:04 -0700

    Fixed bundle erroer

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 VUNDLE_DIR="$HOME/.vim/bundle/vundle"
 
 
-if [ ! -d "{$VUNDLE_DIR}" ];then
+if [ ! -d "$VUNDLE_DIR" ];then
     echo "Cloning Vundle"
     git clone http://github.com/gmarik/vundle.git "$VUNDLE_DIR"
 fi
