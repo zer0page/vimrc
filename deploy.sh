5e09a84297c5547b2d304999e82759b508d25120
Jackson Chen <jackson.jl.chen@gmail.com>
2013-10-02 17:33:50 -0700

    Added set e

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
 VUNDLE_DIR="$HOME/.vim/bundle/vundle"
-
-
 if [ ! -d "$VUNDLE_DIR" ];then
     echo "Cloning Vundle"
     git clone http://github.com/gmarik/vundle.git "$VUNDLE_DIR"
