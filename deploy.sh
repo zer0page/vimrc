7ea9e47fe9bccd07e07b94ba6a37cb3ebda31505
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2016-11-02 15:44:37 -0700

    no hidden

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ if [ ! -d "$VUNDLE_DIR" ];then
     echo "Cloning Vundle"
     git clone http://github.com/gmarik/vundle.git "$VUNDLE_DIR"
 fi
-cp .vimrc ~/
+cp vimrc ~/.vimrc
 vim +BundleInstall +qall
